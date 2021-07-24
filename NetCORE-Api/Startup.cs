aa8cbf3ac646995e2342be615d3e4fda6dfd4712
kane <kkbbg3106@gmail.com>
2021-07-24 22:52:53 +0800

    0724 修改startup

--- NetCORE-Api/Startup.cs
+++ NetCORE-Api/Startup.cs
@@ -34,9 +34,9 @@ namespace WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
             });
-
-            services.AddScoped<IFactory, SimpleFactory>();  
-
+            
+            services.AddScoped<IFactory, SimpleFactory>();   
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
