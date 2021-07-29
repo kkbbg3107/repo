38d74df656b748895aacd64fc6c62ff96cae48dd
kane <kkbbg3106@gmail.com>
2021-07-29 11:17:38 +0800

    移動前端物件

---
+++ ClassLibrary1/Model/Record.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Model
+{
+    /// <summary>
+    /// 靜態類別 負責處理前端給予的資料
+    /// </summary>
+    public static class Record
+    {
+        public static string Btn { get; set; }
+        public static string Lbl { get; set; }
+        public static string TextBoxFirst { get; set; }
+        public static string TextBoxResult { get; set; }
+    }
+}
