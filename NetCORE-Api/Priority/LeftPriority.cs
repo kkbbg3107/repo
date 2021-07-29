d2109a433ae54f5999da8d71e94c12a3346d4bae
kane <kkbbg3106@gmail.com>
2021-07-29 11:06:32 +0800

    移動檔案 priority成功

---
+++ NetCORE-Api/Priority/LeftPriority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCORE_Api.Priority
+{
+    /// <summary>
+    /// 左括號優先權
+    /// </summary>
+    public class LeftPriority : IPriority
+    {
+        public int GetPriority(string c)
+        {
+            return -1;
+        }
+    }
+}
---
+++ NetCORE-Api/Priority/MulDivPriority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCORE_Api.Priority
+{
+    /// <summary>
+    /// 乘號優先權
+    /// </summary>
+    public class MulDivPriority : IPriority
+    {
+        public int GetPriority(string c)
+        {
+            return 9;
+        }
+    }
+}
